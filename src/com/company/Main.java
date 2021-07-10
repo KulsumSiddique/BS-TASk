65f9a3a4310c693d0e920e46558175c77d3d8e86
kulsum <kulsum@groovedigital.com>
2021-07-10 19:28:38 +0600

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,30 +7,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","src\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.phptravels.net/");
+        WebDriverWait wait = new WebDriverWait(driver, 30);
 
-        // To open Gmail site
-        driver.get("https://www.gmail.com");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"search\"]/div/div/div/div/div/nav/ul/li[5]/a"))).click();
 
-
-        //enter gmail credentials
-        driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("test1014409@gmail.com");
-        driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
-
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type=password")));
-        driver.findElement(By.cssSelector("input[type=password]")).sendKeys("vgyuhb852");
-
-        driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button/span")).click();
-
-        //click on compose button
-        Thread.sleep(500);
-        driver.findElement(By.xpath("//div[@class='T-I T-I-KE L3']")).click();
+//        driver.findElement(By.id("select2-drop")).click();
+//        driver.findElement(By.id("select2-drop")).sendKeys("Legoland Malaysia Day Pass");
 
         driver.quit();
     }
